32dac4965d027e9b4b444421993c15e7e0696e99
taehyeong park <b_tae96@naver.com>
2022-06-02 10:33:44 +0900

    finish. 연동 완료 후 마무리

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Route, Routes } from "react-router-dom";
 import BlindSnipet from "./components/Blind_snipet";
 import GlobalStyle from "./elem/GlobalStyle";
+import MainHome from "./features/MainHome";
 import MemoHome from "./features/memo/MemoHome";
+import TodoHome from "./features/Todo/TodoHome";
 import { __getMemos } from "./redux/modules/memos";
-
+import MemoCardList from "./features/memo/MemoCardList";
+import MemoAdd from "./features/memo/MemoAdd";
+import MemoChange from "./features/memo/MemoChange";
+import Header from "./components/Header";
+import { __getRequest } from "./redux/modules/todo";
 function App() {
   const dispatch = useDispatch();
   const { loading, error } = useSelector((state) => state.memos);
   useEffect(() => {
     dispatch(__getMemos());
+    dispatch(__getRequest());
   }, []);
 
   if (loading) {
@@ -21,7 +29,19 @@ function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <MemoHome />
+      <Routes>
+        <Route path="/" element={<MainHome />} />
+        <Route
+          path="/memo"
+          element={<Header name={"나만의 단어장"} color={"var(--green)"} />}
+        >
+          <Route path="" element={<MemoCardList />} />
+          <Route path="add" element={<MemoAdd />} />
+          <Route path="change/:index" element={<MemoChange />} />
+        </Route>
+        {/* todo */}
+        <Route path="/todo" element={<TodoHome />} />
+      </Routes>
     </div>
   );
 }
